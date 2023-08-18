5c5e632d9acd0dc096c3daac1163fcb6484e345f
avalsonigara15 <avals1504@gmail.com>
2023-08-18 23:54:32 +0530

    Etherscan API ~ReadMe Update

--- hardhat_simple_storage/README.md
+++ hardhat_simple_storage/README.md
@@ -20,3 +20,22 @@ npx hardhat run scripts/deploy.js
 2. Use the `await ethers.getContractFactory()` to get the simpleStorage contract.
 3. Deploy the contract using ` await contractfactory.deploy().` and wait for the deployment of the transaction using `await simpleStorage.deploymentTransaction().wait(1)`.
 4. To get the address according to the latest version(v6) need to await for the address to be fetched using `await contract.getAddress()`
+
+### Using API calls to etherscan for verifying the contract everytime
+
+#### Usig _hardhat-verify_ Plugin
+
+**STEPS**
+
+1. Install the _hardhat-verify_ Plugin using ` yarn add --save-dev @nomicfoundation/hardhat-verify` and require it into the hardhat.config.js file
+2. Add the section bellow the networks section
+
+```javascript
+etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+};
+```
+
+3. Create the async Verify function.
+4. Check if the Network Id matches with the name of the network called and the API KEY for etherscan is valid or not and the Call the Verify function.
--- hardhat_simple_storage/scripts/deploy.js
+++ hardhat_simple_storage/scripts/deploy.js
@@ -1,23 +1,47 @@
 // imports
 
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat");
+require("dotenv").config();
 
 // async main function
 async function main() {
-  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
-  // console.log(SimpleStorageFactory);
-  console.log("Deploying contract...");
-  const simpleStorage = await SimpleStorageFactory.deploy()
-  await simpleStorage.deploymentTransaction().wait(1)
-  // console.log(simpleStorage)
-  const address=  await simpleStorage.getAddress()
-  console.log(`Deployed contract to: ${address}`);
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage",
+    );
+    // console.log(SimpleStorageFactory);
+    console.log("Deploying contract...");
+    const simpleStorage = await SimpleStorageFactory.deploy();
+    // await simpleStorage.deploymentTransaction().wait(1);
+    await simpleStorage.waitForDeployment(1);
+    // console.log(simpleStorage)
+    const contractAddress = await simpleStorage.getAddress();
+    console.log(`Deployed contract to: ${contractAddress}`);
+
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.waitForDeployment(10);
+        await verify(contractAddress, []);
+    }
 }
 
+async function verify(contractAddress, args) {
+    console.log("Verifying Contract.....");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified");
+        } else {
+            console.log(e);
+        }
+    }
+}
 // main function
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  })
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
